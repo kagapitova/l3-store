17ace9528ccf1c22b2406e4ab9fddb69e1a3190d
kseniaagapitova <kagapitova@inbox.ru>
2023-11-09 18:35:58 +0200

    fix: review3

--- src/modules/searchTips/searchTips.ts
+++ src/modules/searchTips/searchTips.ts
@@ -28,16 +28,17 @@ export class SearchTips {
                 link: "https://www.wildberries.ru/catalog/0/search.aspx?search=%D1%8F%D0%BD%D0%B4%D0%B5%D0%BA%D1%81%20%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D1%8F%202"
             },
         ];
-
     }
 
     render() {
         const data = this.getData();
         this.view.root.innerHTML = data.reduce(
-          (acc,link, index) => {
-              return acc + `${index === 0 ? 'Например, ' : data.length - 1 === index ? '  или  ' : ', '}<a href="${link.link}">${link.name}</a>`
+          (html,{link, name} , i) => {
+              let tpl = `<a href="${link}">${name}</a>`
+              if(i !== data.length - 1) tpl += (i < data.length - 2) ? ', ' : '  или  '
+              return html + tpl
           },
-          ''
+          'Например, '
         )
     }
 }
